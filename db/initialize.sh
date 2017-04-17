dc7f18d8e7b2e42ad12f8af4f563b1c1448963c1
JZ H <JZ H>
2017-04-16 21:39:04 -0400

    SQL SERVER Data Persistency

--- db/initialize.sh
+++ db/initialize.sh
@@ -1,2 +1,6 @@
 sleep 90s
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Welcome1234! -i setup.sql
+if [ ! -f "/var/opt/mssql/data/CEP.mdf" ]
+then
+    echo "SQLINUX >>>> File does not exists"
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Welcome1234! -i setup.sql
+fi
